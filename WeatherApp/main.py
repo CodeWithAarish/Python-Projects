0621791e91304fcd2f2276802fdd5ed56a768729
CodeWithAarish <aarishm224@gmail.com>
2023-03-28 17:25:30 +0530

    WeatherApp Completed

---
+++ WeatherApp/main.py
@@ -0,0 +1,15 @@
+import requests
+import json
+import win32com.client
+
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+city = input("Enter the name of the city\n")
+
+url = f"https://api.weatherapi.com/v1/current.json?key=01c7974cc345411484c90549232703&q={city}"
+
+r = requests.get(url)
+# print(r.text)
+wdic = json.loads(r.text)
+w = wdic["current"]["temp_c"]
+speaker.Speak(f"The current weather of {city} is {w} degree celsius")
